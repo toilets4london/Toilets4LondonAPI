0dc545f2eb383cf1bfa8293aa85628788a6c2dc1
Nina Rimsky <ninarimsky@gmail.com>
2020-09-15 23:46:55 +0100

    replacing default names / text

--- Toilets4LondonAPI/urls.py
+++ Toilets4LondonAPI/urls.py
@@ -4,8 +4,11 @@ from Toilets4LondonAPI.toilets4london import views
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'toilets', views.ToiletViewSet)
+router.get_api_root_view().cls.__name__ = "LondonToiletsApiRoot"
+router.get_api_root_view().cls.__doc__ = "Browse the Toilets4London API"
+
 router.register(r'users', views.UserViewSet)
+router.register(r'toilets', views.ToiletViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
