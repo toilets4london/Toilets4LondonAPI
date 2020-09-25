7e8136fef7b5703cd0c942b5c29e0e6134b62c54
Nina Rimsky <ninarimsky@gmail.com>
2020-09-25 17:31:47 +0100

    added tests for toilet endpoint

--- Toilets4LondonAPI/toilets4london/tests/toilet_tests.py
+++ Toilets4LondonAPI/toilets4london/tests/toilet_tests.py
@@ -1,34 +1,38 @@
+import json
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from Toilets4LondonAPI.toilets4london.models import Toilet, Toilets4LondonUser
 
 
-def create_toilet():
-
-    user = Toilets4LondonUser.objects.create(
-        email="hello@example.com",
-        password="thisisarandomstring!2"
-    )
-    Toilet.objects.create(
-        address="5 Mill Lane",
-        borough="Camden",
-        latitude=12,
-        longitude=12,
-        owner=user,
-        opening_hours="",
-        name="Fake Toilet",
-        wheelchair=False
-    )
-
-
 class ToiletTests(APITestCase):
 
     def setUp(self):
-        create_toilet()
         self.client = APIClient()
         self.user = self.client.post('/auth/users/',data={'email':'user1@example.com','password':'banana678!'})
         response = self.client.post('/auth/token/login/', data={'email': 'user1@example.com', 'password': 'banana678!'})
         self.token = response.data['auth_token']
         self.client.credentials(HTTP_AUTHORIZATION='TOKEN ' + self.token)
 
-    def test_post_rating_once(self):
+    def test_post_multiple_toilets(self):
+        with open('Toilets4LondonAPI/toilets4london/tests/example_data.json') as datafile:
+            json_data = datafile.read()
+        response = self.client.post(
+            '/toilets/',
+            data=json_data,
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Toilet.objects.count(), 848)
+
+    def test_pagination(self):
+        with open('Toilets4LondonAPI/toilets4london/tests/example_data.json') as datafile:
+            json_data = datafile.read()
+        self.client.post(
+            '/toilets/',
+            data=json_data,
+            content_type='application/json'
+        )
+        response = self.client.get('/toilets/?page_size=1000')
+        self.assertIsNone(response.data['next'])
+        response = self.client.get('/toilets/')
+        self.assertIsNotNone(response.data['next'])
