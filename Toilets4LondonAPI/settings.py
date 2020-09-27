7d26ed173f91cfad6c35b42d31e2ba8c298d0b89
Nina Rimsky <ninarimsky@gmail.com>
2020-09-27 11:06:51 +0100

    removed rogue trailing slash

--- Toilets4LondonAPI/settings.py
+++ Toilets4LondonAPI/settings.py
@@ -170,7 +170,7 @@ if not DEBUG:
 LOGIN_URL = '/admin/login/'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000/",
+    "http://localhost:3000",
     "http://localhost:8080",
     "http://127.0.0.1:8000"
 ]
